f361da19fad9638fe8b59eac08a852e2e31c0b73
Philipp Frenzel <philipp@frenzel.net>
2013-06-15 16:25:30 +0200

    initial upload

---
+++ assets.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+	'yiiJQueryTOC' => array(
+		'sourcePath' => __DIR__ . '/assets',
+		'js' => array(
+			'js/jquery.tableofcontents.js',
+		),
+		'css' => array(
+		),
+		'depends'=>array(
+			'jquery'
+		)
+	)
+);
